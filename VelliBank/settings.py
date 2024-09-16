9304a432a2d7113562193429863f1c168446676b
D-Velli <mamadoudiallo0508@gmail.com>
2024-09-16 00:23:51 -0400

    Update model members avec AbstractBaseUser

--- VelliBank/settings.py
+++ VelliBank/settings.py
@@ -10,23 +10,24 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(dotenv_path=Path(BASE_DIR / '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0i0de4&-2sq4457+z-v_r1eyqn)383tu^n6)2+!mu(e&v^hyxd'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ["127.0.0.1"]
-
+DEBUG = os.getenv('DEBUG')
 
 # Application definition
 
@@ -74,21 +75,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'VelliBank.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'sMjyHvbvroFJkyScEsiATsdqGzDvlVHs',
-        'HOST': 'autorack.proxy.rlwy.net',
-        'PORT': '32481',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -108,7 +107,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
@@ -120,7 +118,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
@@ -129,8 +126,9 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'members.Client'
--- members/models.py
+++ members/models.py
@@ -1,10 +1,32 @@
 from datetime import timedelta, date
-from distutils.log import fatal
-from django.contrib.auth.models import User
-from django.utils import timezone
+
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+# from django.contrib.auth.hashers import make_password
+# from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from django.db import models
-from django.contrib.auth.hashers import make_password
+from django.utils import timezone
+
+
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError('Vous etes oblige de rentrer un adresse email')
+
+        user = self.model(email=self.normalize_email(email))
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password=None):
+        user = self.create_user(email=email, password=password)
+        user.is_staff = True
+        user.is_admin = True
+        user.save()
+        return user
+
+
+
 
 
 class Adresse(models.Model):
@@ -20,47 +42,58 @@ class Adresse(models.Model):
         return f"{self.app_unite} {self.rue} {self.ville} {self.province} {self.code_postal}"
 
 
-class Client(models.Model):
+class Client(AbstractBaseUser):
     STATUS_CHOICES = [
-        ("active", "Active"), # l'utilisateur est membre et son compte est actif
-        ("inactive", "Inactive"), # L'utilisateur a son compte desactive temporairement
-        ("visiteur", "Visiteur"), # Le compte de l'utilisateur n'est pas encore confirme, c'est un simple visiteur a ce stade
-        ("bloque", "Bloque"), # Le compte de l'utilisateur est bloque, il ne peut faire aucune transaction
+        ("active", "Active"),  # l'utilisateur est membre et son compte est actif
+        ("inactive", "Inactive"),  # L'utilisateur a son compte desactive temporairement
+        ("visiteur", "Visiteur"),
+        # Le compte de l'utilisateur n'est pas encore confirme, c'est un simple visiteur a ce stade
+        ("bloque", "Bloque"),  # Le compte de l'utilisateur est bloque, il ne peut faire aucune transaction
     ]
 
     nom = models.CharField(max_length=100)
     prenom = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=100)
+    email = models.EmailField(unique=True, blank=False, max_length=254)
     numero = models.CharField(unique=True)
     date_inscription = models.DateField(auto_now_add=True)
     date_naissance = models.DateField(null=False, blank=False, default=date(2004, 1, 1))
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="visiteur")
     adresse = models.ForeignKey(Adresse, on_delete=models.SET_NULL, null=True, blank=True)
-    genre = models.CharField(max_length=100, choices=[("M", "Masculin"), ("F", "Feminin"), ("X", "Sac Walmart")], default="Masculin")
+    genre = models.CharField(max_length=100, choices=[("M", "Masculin"), ("F", "Feminin"), ("X", "Sac Walmart")],
+                             default="Masculin")
     is_first = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)  # Un user par defaut n'est pas un admin
+    is_staff = models.BooleanField(default=False)  # Un user par defaut ne fait pas partit du groupe d'admin
+    is_active = models.BooleanField(default=True)  # Un User par defaut est active
+    objects = MyUserManager()
+
+    USERNAME_FIELD = "email"  # Le champ qu'on utilise pour ce connecter
+    # REQUIRED_FIELDS = ["nom", "prenom", "date_naissance", "genre"]  # Les champs obligatoire
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, perm, obj=None):
+        return True
 
     class Meta:
         ordering = ['nom']
 
-    #Le schema d'affichage de mes donnees
+    # Le schema d'affichage de mes donnees
     def __str__(self):
         return f'{self.nom}, {self.prenom} ({self.status}) {str(self.date_inscription)}'
 
-    #HAchage du mdp avant la sauvegarde
-    def save(self, *args, **kwargs):
-        if self.password and not self.password.startswith("pbkdf2_sha256$"):
-            self.password = make_password(self.password)
+    # # HAchage du mdp avant la sauvegarde
+    # def save(self, *args, **kwargs):
+    #     if self.password and not self.password.startswith("pbkdf2_sha256$"):
+    #         self.password = make_password(self.password)
+    #
+    #     super().save(*args, **kwargs)
 
-        super().save(*args, **kwargs)
-
-    #Verifier si l'utilisateur qui s'inscrit est majeur. Autrepasse si un admin qui ouvre le compte
+    # Verifier si l'utilisateur qui s'inscrit est majeur. Autrepasse si un admin qui ouvre le compte
     def clean(self):
         super().clean()
         if self.date_naissance:
-            age_limit = timezone.now().date() - timedelta(days=18*365)
+            age_limit = timezone.now().date() - timedelta(days=18 * 365)
             if self.date_naissance > age_limit:
                 raise ValidationError("Vous devez avoir au moins 18 ans pour ouvrir un compte.")
-
-
-
